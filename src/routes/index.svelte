b21d00a7be8b8b4391e4d82282f3c1e40f3d8f62
lucasg2155 <ggcon.gabriel@gmail.com>
2020-01-29 23:56:16 -0300

    myapp 3 commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -32,6 +32,22 @@
 	}
 </style>
 
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  });
+</script>
+
 <svelte:head>
 	<title>Sapper project template</title>
 	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
