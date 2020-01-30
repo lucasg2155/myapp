c8bf517274773eac7205493528add80f6abdc79c
lucasg2155 <ggcon.gabriel@gmail.com>
2020-01-30 01:38:36 -0300

    myapp 4 commit

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<!-- <script context="module">
 	export async function preload({ params, query }) {
 		// the `slug` parameter is available because
 		// this file is called [slug].svelte
@@ -16,6 +16,41 @@
 <script>
 	export let post;
 </script>
+-->
+
+<script context="module">                                                                                                                                                                                                                                                                   
+  export async function preload({ params, query }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await this.fetch(`_posts/${params.slug}.md`);
+
+    if (res.status === 200) {
+      return { postMd: await res.text() };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  import fm from 'front-matter';
+  import MarkdownIt from 'markdown-it';
+
+  export let postMd;
+
+  const md = new MarkdownIt();
+
+  $: frontMatter = fm(postMd);
+  $: post = {
+    ...frontMatter.attributes,
+    html: md.render(frontMatter.body)
+  };
+</script>
+
+
+
+
+
 
 <style>
 	/*
